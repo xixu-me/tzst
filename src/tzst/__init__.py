bca4bfaea83bc4a87351269c4b87c154ef45e733
Xi Xu <i@xi-xu.me>
2025-05-31 17:15:19 +0800

    Update dependencies and remove compatibility fix
    Updated the zstandard dependency version in pyproject.toml to allow versions starting from 0.23.0. Removed a compatibility fix for Python 3.14 in src/tzst/__init__.py as it is no longer needed.

--- src/tzst/__init__.py
+++ src/tzst/__init__.py
@@ -1,16 +1,5 @@
 """tzst - A Python library for creating and manipulating .tzst/.tar.zst archives."""
 
-# Compatibility fix for Python 3.14 - ByteString was removed from typing
-# This is required for the zstandard library to work with Python 3.14+
-import typing
-
-try:
-    from collections.abc import ByteString
-
-    typing.ByteString = ByteString
-except ImportError:
-    pass
-
 __version__ = "0.4.0"
 
 from .core import (

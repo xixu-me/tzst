eb4d29e5031495a0c8cbb0139b4997b5a241bc52
Xi Xu <i@xi-xu.me>
2025-06-04 21:27:09 +0800

    Bump version to 1.2.2
    Updated the `__version__` in `__init__.py` from 1.2.1 to 1.2.2 to reflect the latest changes or release.

--- src/tzst/__init__.py
+++ src/tzst/__init__.py
@@ -4,7 +4,7 @@ Leveraging cutting-edge Zstandard compression to deliver superior performance,
 security, and reliability.
 """
 
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 from .core import (
     TzstArchive,
